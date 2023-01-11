7bd1f1ad8ee7001bbf9ddb38406a1e18740014dd
CXF114514 <122069395+CXF114514@users.noreply.github.com>
2023-01-11 20:00:45 +0800

    Add files via upload

---
+++ search.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+<?php
+if($_POST["search"]==="game")
+{    
+	$url="game";
+}    
+elseif($_POST["search"]==="testpage")
+{    
+	$url="test";
+}
+else
+{
+	$url="index.html";
+}
+?>
+<meta http-equiv="refresh" content="1;url=<?php echo $url;?>"/>
+</head>
+<body>
+	<h1>正在加载……</h1>
+</body>
+</html>
